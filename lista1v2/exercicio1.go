dfe685da9a222065f0a58577aae56f364d234c59
Maria de Andrade <mariaclaradeandradesilva11@gmail.com>
2026-03-25 03:38:49 -0300

    exercício 1 Go

--- lista1v2/exercicio1.go
+++ lista1v2/exercicio1.go
@@ -17,11 +17,23 @@ package main
 
 import "fmt"
 
-func main() {
-	var nota1, nota2, nota3 float32
-	fmt.Println("Quais são suas notas? (Digite-as espaçadamente: 10 7 6)")
-	fmt.Scan(&nota1, &nota2, &nota3)
-}
-func media(nota1, nota2, nota3 float32) float32 {
+func media(nota1, nota2, nota3 float64) float64 {
 	return (nota1 + nota2 + nota3) / 3
 }	
+
+func main() {
+	var nota1, nota2, nota3 float64
+	var med float64
+	fmt.Println("Quais são suas notas? (Digite-as espaçadamente: 10 7 6)")
+	fmt.Scan(&nota1, &nota2, &nota3)
+	for (nota1 > 10 || nota2 > 10 || nota3 > 10 || nota1 < 0 || nota2 < 0 || nota3 < 0) {
+		fmt.Println("Digite valores válidos, ou seja, maiores que zero e menores que dez.")
+		fmt.Scan(&nota1, &nota2, &nota3)
+	}
+	med = media(nota1, nota2, nota3)   
+	if med < 6{
+		fmt.Println("Você está reprovado.")
+	}else{
+		fmt.Println("Você está aprovado.")
+	}
+}
