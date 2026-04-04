4f405e0fac89b8f8a67afedc5c085ec77f783ead
Maria de Andrade <mariaclaradeandradesilva11@gmail.com>
2026-04-04 00:36:01 -0300

    still needs improvement

--- lista02_Go/exercicio16.go
+++ lista02_Go/exercicio16.go
@@ -4,6 +4,9 @@
 classificação das mesmas: “RAÍZES IMAGINÁRIAS”, “RAIZ ÚNICA” ou “RAÍZES DISTINTAS”.
 */
 
+
+//vou terminar depois, não consigo raciocinar mais 
+
 package main 
 
 import (
@@ -12,15 +15,25 @@ import (
 )
 
 func main() {
-	var a, b, c int
-	var x complex64
+	var a, b, c float64
+	var x1, x2  complex64
+	var delta, w float64
 	fmt.Println("Este programa utiliza a seguinte equação quadrática: ( A*x*x + B*x + C = 0).\nDigite os valores dos coeficientes A, B e C.")
 	fmt.Scan(&a, &b, &c)
-	x = calculo(a, b, c)
-	if x 
-	fmt.Printf("Os resultados da equação são: %v.")
+	delta = (b*b - 4*a*c)
+	w = math.Sqrt(delta)
+
+	if delta > 0{
+		x1 = ((-b + w)/ 2*a)
+		x2 = ((-b - w)/ 2*a)
+		fmt.Printf("As raizes são %v e %v.", x1, x2)
+	}else if delta == 0{
+		x1 = (-b / 2*a)
+		fmt.Printf("As duas raízes são iguais, e seu valor é %v.", x1)
+	}else{
+		x1 = ((-b + w)/ 2*a)
+		x2 = ((-b - w)/ 2*a)
+		fmt.Println("Há duas raízes complexas, e seus valores são %v e %v.", x1, x2)
+	}
 }
 
-func calculo(a, b, c int) complex64 {
-	return ((a*x*x + b*x + c) = 0)
-}
